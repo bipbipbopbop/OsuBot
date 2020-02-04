90660be56fbb802eaa5991c58b8e676a7cde2bcd
julienhache <julien.hache7@gmail.com>
2020-02-04 19:29:08 +0100

    Update build.sh for msys2 toolchains support.

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-cmake CMakeLists.txt -B build -DENABLE_TESTS=ON
+checkMSYS=${MSYSTEM}
+if [[ -z "$checkMSYS" ]]; then
+    cmake CMakeLists.txt -B build -DENABLE_TESTS=ON
+elif [[ "$checkMSYS" == "MINGW64" ]] || [[ "$checkMSYS" == "MINGW32" ]]; then
+    cmake -G "MSYS Makefiles" CMakeLists.txt -B build -DENABLE_TESTS=ON
+fi
 make -C build
 cp build/OsuBot .
